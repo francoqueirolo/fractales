e13c6f7818b390942dc1b96b1453d07101940a7d
Franco Queirolo <fgqueirolo@gmail.com>
2020-06-18 00:44:26 -0500

    feat: adding iterations control and canvas holder

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-function drawFractral(center, vertexCount, radius, iteration) {
+function drawFractral(center, vertexCount, radius, reductor, iteration) {
   if (iteration <= 0) return;
 
   let alpha = 0;
@@ -20,12 +20,13 @@ function drawFractral(center, vertexCount, radius, iteration) {
   endShape();
 
   for (const item of listVertex) {
-    drawFractral(item, vertexCount, radius / 2, iteration - 1);
+    drawFractral(item, vertexCount, radius * reductor, reductor, iteration - 1);
   }
 }
 
 function setup() {
-  createCanvas(640, 480);
+  let canvas = createCanvas(640, 480);
+  canvas.parent("canvas-holder");
 }
 
 function draw() {
@@ -33,14 +34,17 @@ function draw() {
 
   noFill();
   stroke(255);
-  drawFractral({ x: width / 2, y: height / 2 }, sides, radio, 5);
+  drawFractral({ x: width / 2, y: height / 2 }, sides, radio, 0.75, interations);
 }
 
 let sides = 5;
 let radio = 100;
+let interations = 2;
 
 const inputVertex = document.querySelector(".input-vertex");
 const inputRadio = document.querySelector(".input-radio");
+const inpuIterations = document.querySelector(".input-iterations");
 
 inputVertex.addEventListener("change", (event) => (sides = inputVertex.value));
-inputRadio.addEventListener("change", (event) => (radio = inputRadio.value));
+inputRadio.addEventListener("change", (event) => (radio = inputRadio.value * 10));
+inpuIterations.addEventListener("change",(event) => (interations = inpuIterations.value));
