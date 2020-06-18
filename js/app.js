53bf91dc50b6bc6854736608b61b65d1fbb8d0a3
Franco Queirolo <fgqueirolo@gmail.com>
2020-06-18 00:15:01 -0500

    feat: adding iteration to stop recursivity

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-function drawFractral(center, vertexCount, radius) {
+function drawFractral(center, vertexCount, radius, iteration) {
+  if (iteration <= 0) return;
+
   let alpha = 0;
   const alphaStep = (PI * 2) / vertexCount;
+  const listVertex = [];
 
   beginShape();
 
@@ -10,10 +13,15 @@ function drawFractral(center, vertexCount, radius) {
       y: center.y + Math.sin(alpha) * radius,
     };
     alpha += alphaStep;
+    listVertex.push(v);
     vertex(v.x, v.y);
   }
 
   endShape();
+
+  for (const item of listVertex) {
+    drawFractral(item, vertexCount, radius / 2, iteration - 1);
+  }
 }
 
 function setup() {
@@ -25,14 +33,14 @@ function draw() {
 
   noFill();
   stroke(255);
-  drawFractral({ x: width / 2, y: height / 2 }, sides, radio);
+  drawFractral({ x: width / 2, y: height / 2 }, sides, radio, 5);
 }
 
 let sides = 5;
 let radio = 100;
 
 const inputVertex = document.querySelector(".input-vertex");
-const inputRadio= document.querySelector(".input-radio");
+const inputRadio = document.querySelector(".input-radio");
 
 inputVertex.addEventListener("change", (event) => (sides = inputVertex.value));
 inputRadio.addEventListener("change", (event) => (radio = inputRadio.value));
